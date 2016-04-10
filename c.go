9136b04e74615de4ba3a7161d3dea695aa5cc79d
Christian Wellenbrock <christian.wellenbrock@gmail.com>
2016-04-10 13:40:08 +0200

    Use values array for C

--- c.go
+++ c.go
@@ -35,99 +35,56 @@ func main() {
 }
 
 var (
+	bases  = []int{2, 3, 4, 5, 6, 7, 8, 9, 10}
 	primes = []int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47}
 	P      = 614889782588491410 // product of primes above
 )
 
 func c(N, J int) {
-	iterate(N, J, 0, 0, 0, 0, 0, 0, 0, 0, 0, "", true)
+	iterate(N, J, []int{0, 0, 0, 0, 0, 0, 0, 0, 0}, "", true)
 }
 
-func iterate(left, limit, v2, v3, v4, v5, v6, v7, v8, v9, v10 int, s string, one bool) int {
+func iterate(left, limit int, values []int, s string, one bool) int {
 	if limit == 0 {
 		return 0
 	}
 
 	if left == 0 {
-		f2 := factor(v2)
-		if f2 == 0 {
-			return 0
+		factors := []int{}
+		for i, _ := range bases {
+			factors = append(factors, factor(values[i]))
+			if factors[i] == 0 {
+				return 0
+			}
 		}
 
-		f3 := factor(v3)
-		if f3 == 0 {
-			return 0
+		fmt.Printf("%s", s)
+		for _, factor := range factors {
+			fmt.Printf(" %d", factor)
 		}
-
-		f4 := factor(v4)
-		if f4 == 0 {
-			return 0
-		}
-
-		f5 := factor(v5)
-		if f5 == 0 {
-			return 0
-		}
-
-		f6 := factor(v6)
-		if f6 == 0 {
-			return 0
-		}
-
-		f7 := factor(v7)
-		if f7 == 0 {
-			return 0
-		}
-
-		f8 := factor(v8)
-		if f8 == 0 {
-			return 0
-		}
-
-		f9 := factor(v9)
-		if f9 == 0 {
-			return 0
-		}
-
-		f10 := factor(v10)
-		if f10 == 0 {
-			return 0
-		}
-
-		fmt.Printf("%s %d %d %d %d %d %d %d %d %d\n", s, f2, f3, f4, f5, f6, f7, f8, f9, f10)
+		fmt.Printf("\n")
 		return 1
 	}
 
-	v2 = v2 * 2 % P
-	v3 = v3 * 3 % P
-	v4 = v4 * 4 % P
-	v5 = v5 * 5 % P
-	v6 = v6 * 6 % P
-	v7 = v7 * 7 % P
-	v8 = v8 * 8 % P
-	v9 = v9 * 9 % P
-	v10 = v10 * 10 % P
+	v := []int{}
+	for i, b := range bases {
+		v = append(v, values[i]*b%P)
+	}
 
-	if one {
-		v2++
-		v3++
-		v4++
-		v5++
-		v6++
-		v7++
-		v8++
-		v9++
-		v10++
+	if one { // TODO: make string
+		for i, _ := range v {
+			v[i]++
+		}
 		s += "1"
 	} else {
 		s += "0"
 	}
 
 	left--
-	found := iterate(left, limit, v2, v3, v4, v5, v6, v7, v8, v9, v10, s, true)
+	found := iterate(left, limit, v, s, true)
 	limit -= found
 	if left > 1 {
-		found += iterate(left, limit, v2, v3, v4, v5, v6, v7, v8, v9, v10, s, false)
+		found += iterate(left, limit, v, s, false)
 	}
 	return found
 }
